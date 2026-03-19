cbd6493a37a7b2d5daa69f34a07d89e06b9b0ea3
Bhavpreet Singh <bhavpreet@gmail.com>
2026-03-19 07:33:47 +0530

    chore: update cask to v1.0.1 (latest release)

--- Casks/soundvibe.rb
+++ Casks/soundvibe.rb
@@ -1,5 +1,5 @@
 cask "soundvibe" do
-  version "1.0.0"
+  version "1.0.1"
   sha256 "35ccdcea24430fe1292796218e321b1092f4891c5d28e98c3b0f32f4cc396eb3"
 
   url "https://github.com/bhavpreet/SoundVibe/releases/download/v#{version}/SoundVibe.dmg"
